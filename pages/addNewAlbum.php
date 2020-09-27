80310c2542f690ae7fd0b4846fd755000ee765fe
Felipe <felipelsouza@unipam.edu.br>
2020-09-27 16:17:04 -0300

    Form validations with JS

--- pages/addNewAlbum.php
+++ pages/addNewAlbum.php
@@ -4,11 +4,11 @@
 <div class="row">
     <div class="col-md-6 col-md-offset-1">
         <div class="well well-sm">
-            <form action="server/newAlbum.php" method="post" class="form-horizontal" novalidate>
+            <form action="server/newAlbum.php" method="post" class="form-horizontal" onsubmit="return validateForm();" novalidate>
                 <fieldset>
                     <legend class="text-center">Insert Album's info</legend>
                     <div class="form-group">
-                        <label for="name" class="col-md-12 control-label">Name</label>
+                        <label for="name" class="col-md-12 control-label" id="albumNameLabel">Name</label>
                         <div class="col-md-12">
                             <input class="form-control" id="albumName" 
                                 type="text" 
@@ -79,3 +79,40 @@
         </div>
     </div>
 </div>
+
+<script>
+    function validateForm () {
+        var albumName = document.getElementById('albumName');
+        var band = document.getElementById('band');
+        var releaseDate = document.getElementById('releaseDate');
+        var price = document.getElementById('price');
+        
+        var currentDate = new Date();
+
+        if(albumName.value == '' || albumName.value === null || albumName.value.lenght < 1) {
+            alert('Name of the Album cannot be blank');
+            albumName.focus();
+            return false;
+        }
+
+        if(band.value == '' || band.value === null || band.value.lenght < 1) {
+            alert('Band cannot be blank');
+            band.focus();
+            return false;
+        }
+
+        if(releaseDate.value == '' || releaseDate.value === null) {
+            alert('Release Date cannot be blank');
+            releaseDate.focus();
+            return false;
+        }
+
+        if(price.value == '' || price.value == null || price.value <= 0) {
+            alert('Price must be greater than 0');
+            price.focus();
+            return false;
+        }
+
+        return true;
+    }
+</script>
